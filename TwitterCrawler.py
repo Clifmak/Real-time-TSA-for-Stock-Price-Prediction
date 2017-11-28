3f39e2baa37f7db3257ddcb1917e857f1075d154
Clifmak <cmm9@williams.edu>
2017-11-27 20:34:52 -0500

    Update TwitterCrawler.py

--- TwitterCrawler.py
+++ TwitterCrawler.py
@@ -1,15 +1,15 @@
 #Twitter Crawler Template by Cliff Makanda
-import tweepy
+import tweepy #https://github.com/tweepy/tweepy
 import csv
 import pandas as pd
 
-#Credentials
+#twitter API credentials
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
 
-#Authentication 
+#authorize twitter and initialize tweppy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
@@ -17,3 +17,9 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 #CVS File for tweets
 csvFile = open('ua.csv', 'a')
 csvWriter = csv.writer(csvFile)
+
+for tweet in tweepy.Cursor(api.search,q="#AMZN",count=100,
+                           lang="en",
+                           since="2017-04-03").items():
+    print (tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])

eb9a6d1a51f7a9eb7fffb6805b43f95158ff4c1d
Clifmak <cmm9@williams.edu>
2017-11-27 19:56:53 -0500

    Update TwitterCrawler.py

--- TwitterCrawler.py
+++ TwitterCrawler.py
@@ -1,1 +1,19 @@
 #Twitter Crawler Template by Cliff Makanda
+import tweepy
+import csv
+import pandas as pd
+
+#Credentials
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+#Authentication 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+#CVS File for tweets
+csvFile = open('ua.csv', 'a')
+csvWriter = csv.writer(csvFile)

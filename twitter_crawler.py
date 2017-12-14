efd308467a733706931baa41c96a8d67a404c201
Clifmak <cmm9@williams.edu>
2017-12-14 18:44:58 -0500

    Update twitter_crawler.py

--- twitter_crawler.py
+++ twitter_crawler.py
@@ -26,26 +26,29 @@ for tweet in tweepy.Cursor(api.search,q="#NVDA",count=10,
     print (tweet.created_at, tweet.text)
     csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
 '''
-
-for company in ["#AMZN", "#APPL", "#MSFT", "#TSLA"]:
-    neg_score = 0
-    pos_score = 0
-    for tweet in tweepy.Cursor(api.search,q="#"+company,
+targetList = ["WSJ", "nytimes", "business", "CNBC"]
+companyList = ["#ADP", "#ADBE", "#MSFT", "#TSLA"]
+for company in companyList :
+    for name in targetList :
+        neg_score = 0
+        pos_score = 0
+        for tweet in tweepy.Cursor(api.search,q="#"+company, sceen_name = name
                            lang="en",
-                           since="2017-11-28").items(50):
-        blob = textblob.TextBlob(tweet.text, analyzer=textblob.sentiments.NaiveBayesAnalyzer())
-        neg_score += blob.sentiment.p_neg
-        pos_score += blob.sentiment.p_pos
+                           since="2017-11-28").items(10):
+            blob = textblob.TextBlob(tweet.text, analyzer=textblob.sentiments.NaiveBayesAnalyzer())
+            neg_score += blob.sentiment.p_neg
+            pos_score += blob.sentiment.p_pos
+            csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+        neg_avg = neg_score/10
+        pos_avg = pos_score/10
+        if (neg_avg > pos_avg):
+            sent = "negative"
+            diff = neg_avg-pos_avg
+        elif (pos_avg > neg_avg):
+            sent = "positive"
+            diff = pos_avg-neg_avg
+        else:
+            sent = "neutral"
+            diff = 0
+        print (company, sent, diff)
         csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-    neg_avg = neg_score/50
-    pos_avg = pos_score/50
-    if (neg_avg > pos_avg):
-        sent = "negative"
-        diff = neg_avg-pos_avg
-    elif (pos_avg > neg_avg):
-        sent = "positive"
-        diff = pos_avg-neg_avg
-    else:
-        sent = "neutral"
-        diff = 0
-    print (company, sent, diff)

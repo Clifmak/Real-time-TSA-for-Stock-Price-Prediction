96f1f06d1bbe307cc040cc923c26442d4413cd1e
Clifmak <cmm9@williams.edu>
2017-12-15 13:20:41 -0500

    Update filterTweets.py

--- filterTweets.py
+++ filterTweets.py
@@ -58,7 +58,7 @@ for row in tweetFrame :
   featureList.extend(featureVector)
   tweets.append((featureVector, sentiment))
 featureList = list(set(featureList))
-training_set = nltk.classify.util.apply_features(extract_features, tweets)
+training_set = nltk.classify.util.apply_features(featureList, tweets)
 print(training_set)
   
 

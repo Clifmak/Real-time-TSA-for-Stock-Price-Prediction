ca7ffab2f226452f6161e2ec5333c78834ca2624
Clifmak <cmm9@williams.edu>
2017-12-15 10:59:50 -0500

    Update featureExtraction.py

--- featureExtraction.py
+++ featureExtraction.py
@@ -1,7 +1,9 @@
+#Adapted from NLTK Twitter Data Sentiment Analysis
 tweetFrame = csv.reader('Sentiment Analysis Dataset.csv', 'rb')
 tweets = []
 for row in tweetFrame :
   sentiment = row[0]
   tweet = row[1]
+  processedTweet = preprocessing
   
   
